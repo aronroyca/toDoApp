9262fa72b708521a879754ed1eb90a421dd025d0
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-11-27 20:38:19 -0800

    added an input to the html

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var serveStatic = require('serve-static');
 
 http
   .createServer(function (req, res) {
-    fs.readFile(
-      "./public/index.html",
-      { encoding: "utf-8" },
-      function (err, data) {
+    fs.readFile('./public/index.html', { encoding: 'utf-8' }, function (err, data) {
         if (err) {
           console.log(err);
         } else {
           res.end(data);
         }
-      }
-    );
+      });
   })
   .listen(3000);
