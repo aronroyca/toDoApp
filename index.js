3534055e650c3c5194f58faf3d6e48305d74a78d
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-12-11 17:58:46 -0800

    fixed the basic routing

--- index.js
+++ index.js
@@ -9,17 +9,20 @@ http
         if (err) {
           console.log(err);
         } else {
+          console.log('success')
           res.end(data);
         }
       });
     } else if (req.url === '/signin.html') {
-      var path = './todolist.html'
-      // res.on('data', function (data) {
-      //   console.log(data);
-        res.end(path)
-      // })
-      // var path = path.join(__dirname, req)
-      // var list = fs.readFile()
+      var path = './public/todolist.html'
+      fs.readFile(path, { encoding: 'utf-8' }, function (err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('success')
+          res.end(data);
+        }
+      });
     }
   })
   .listen(3000);
