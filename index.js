316e97ea7a62112e256d1dfea0b9f4808005a69a
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-12-22 11:44:42 -0800

    made route handlers using express and bp

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+let express = require('express');
+let bp = require('body-parser');
 
-http
-  .createServer(function (req, res) {
-    if (req.url === '/') {
-          fs.readFile('./public/index.html', { encoding: 'utf-8' }, function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('success')
-          res.end(data);
-        }
-      });
-    } else if (req.url === '/signin.html') {
-      var path = './public/todolist.html'
-      fs.readFile(path, { encoding: 'utf-8' }, function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('success')
-          res.end(data);
-        }
-      });
-    }
-  })
-  .listen(3000);
+const app = express();
+app.use(bp.urlencoded({ extended: true }))
+app.use(bp.json())
+
+let todos = [
+  'clean bathroom',
+  'add database to todo app',
+  'wrap christmas gifts'
+]
+
+app.use(express.static('./public'));
+
+app.get('/home', function (req, res) {
+  console.log('get /home');
+  res.json(todos);
+});
+
+app.post('/home', function (req, res) {
+  console.log('post /home', req.body.text);
+  todos.push(req.body.text)
+  res.json(todos);
+});
+
+app.delete('/home', function (req, res) {
+  console.log('post /home', req.body.text);
+  todos = todos.filter(x => x !== req.body.text)
+  res.json(todos);
+});
+
+app.listen(3000)
