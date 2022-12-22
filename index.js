8a498f6c50df1434d9d88a59d0520f0774992b1b
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-12-22 13:22:08 -0800

    working on route handlers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let fs = require('fs')
 let express = require('express');
 let bp = require('body-parser');
 
@@ -5,28 +6,35 @@ const app = express();
 app.use(bp.urlencoded({ extended: true }))
 app.use(bp.json())
 
-let todos = [
-  'clean bathroom',
-  'add database to todo app',
-  'wrap christmas gifts'
-]
+let todos = fs.readFileSync('./todos.txt', 'utf-8', (err, data) => {
+  if (err) console.log(err)
+  console.log(data);
+});
 
-app.use(express.static('./public'));
 
-app.get('/home', function (req, res) {
+// app.use(express.static('./public'));
+
+app.get('/home', (req, res) => {
   console.log('get /home');
   res.json(todos);
 });
 
-app.post('/home', function (req, res) {
+app.post('/home', (req, res) => {
   console.log('post /home', req.body.text);
-  todos.push(req.body.text)
-  res.json(todos);
+  fs.appendFileSync('./todos.txt', `, ${req.body.text}`, 'utf-8');
+  todos = fs.readFileSync('./todos.txt', 'utf-8', (err, data) => {
+  if (err) console.log(err)
+  console.log(data);
 });
+  res.json(todos);
+  });
 
-app.delete('/home', function (req, res) {
+app.delete('/home', (req, res) => {
   console.log('post /home', req.body.text);
-  todos = todos.filter(x => x !== req.body.text)
+  todos = todos.split(',').filter(x => x !== req.body.text).join(',')
+  fs.writeFileSync('./todos.txt', `, ${req.body.text}`, (err) => {
+    if (err) console.log(err)
+  })
   res.json(todos);
 });
 
